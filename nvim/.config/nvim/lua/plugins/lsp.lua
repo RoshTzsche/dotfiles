f8f2d27605e2d58b04ac343cb8ac23ae13a8ca56
RoshTzsche <roshguadiana@gmail.com>
2026-03-28 14:52:59 -0600

    tinymist = nvim

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -6,71 +6,66 @@ return {
     "hrsh7th/cmp-nvim-lsp", -- El puente vital para autocompletado
   },
   config = function()
-    -- 1. Iniciar Mason (El Gestor de Binarios)
-    require("mason").setup()
-    
-    -- 2. Declarar las capacidades (Capabilities) para CMP
+    -- 1. THE VOCABULARY (Capabilities)
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    -- 3. Iniciar Mason-LSPConfig (La lista de instalación)
+    -- ==========================================
+    -- 2. THE BLUEPRINTS (Native Declarations)
+    -- ==========================================
+
+-- A) Define Python (Pyright) natively
+vim.lsp.config('pyright', {
+  capabilities = capabilities,
+  settings = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = "workspace",
+      },
+    },
+  },
+})
+
+-- B) Define Typst (Tinymist) natively
+vim.lsp.config('tinymist', {
+  capabilities = capabilities,
+  settings = {
+    exportPdf = "onSave",
+    outputPath = "$root/$dir/$name",
+    formatterMode = "typstyle",
+    semanticTokens = "disable"
+  }
+})
+
+-- C) Define Lua natively
+vim.lsp.config('lua_ls', {
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = { globals = { "vim" } },
+    },
+  },
+})
+    -- [!] ASSEMBLE HERE: Paste the vim.lsp.config('pyright', ...) block
+    
+    -- [!] ASSEMBLE HERE: Paste the vim.lsp.config('tinymist', ...) block
+    
+    -- [!] ASSEMBLE HERE: Paste the vim.lsp.config('lua_ls', ...) block
+
+    -- ==========================================
+    -- 3. THE IGNITION (Mason & Bridge)
+    -- ==========================================
+    require("mason").setup()
+    
     require("mason-lspconfig").setup({
       ensure_installed = { "pyright", "lua_ls", "tinymist" },
+      -- automatic_enable = true is implied in v2.0.0+
     })
 
-    -- 4. CONFIGURACIÓN EXACTA DE SERVIDORES (Handlers)
-    require("mason-lspconfig").setup_handlers({
-      
-      -- A) Handler por defecto (Para cualquier LSP sin config especial)
-      function(server_name)
-        require("lspconfig")[server_name].setup({
-          capabilities = capabilities,
-        })
-      end,
-
-      -- B) Handler para PYTHON
-      ["pyright"] = function()
-        require("lspconfig").pyright.setup({
-          capabilities = capabilities,
-          settings = {
-            python = {
-              analysis = {
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
-                diagnosticMode = "workspace",
-              },
-            },
-          },
-        })
-      end,
-
-      -- C) Handler para TYPST (La Visión de Zathura)
-      ["tinymist"] = function()
-        require("lspconfig").tinymist.setup({
-          capabilities = capabilities,
-          settings = {
-            exportPdf = "onSave",
-            outputPath = "$root/$dir/$name",
-            formatterMode = "typstyle",
-            semanticTokens = "disable"
-          }
-        })
-      end,
-
-      -- D) Handler para LUA
-      ["lua_ls"] = function()
-        require("lspconfig").lua_ls.setup({
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              diagnostics = { globals = { "vim" } },
-            },
-          },
-        })
-      end,
-    })
-
-    -- 5. ATAJOS DE TECLADO (LspAttach)
-    -- Solo se activan cuando un LSP se conecta exitosamente a tu buffer
+    -- ==========================================
+    -- 4. THE INTERACTIVITY (Keybindings)
+    -- ==========================================
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("UserLspConfig", {}),
       callback = function(ev)
@@ -81,8 +76,6 @@ return {
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
         -- Renombrar variable
         vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
-        -- Acciones de código
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
       end,
     })
   end,
